3a4567a67bf1bdcbb3508ff1076942ce8067837c
arminioro <robersalessandro@gmail.com>
2026-04-17 10:55:28 -0400

    creando proyecto

---
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: usuario
+ * Fecha: 17/4/2026
+ * Hora: 10:52 a. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace TallerIUJO01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
